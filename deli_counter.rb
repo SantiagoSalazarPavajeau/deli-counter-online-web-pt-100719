37c3512a2794549ae2f40b10e5d2c6f6ecd453ab
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 12:16:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 require "pry"
 def line(katz_deli)
-  current_line = []
   if katz_deli.empty?                   # if the array is empty it prints the line is empty
   puts "The line is currently empty."
-  else katz_deli.any?
-  other_deli.each_with_index do |people, index| 
-     customers= "The line is currently: #{index+1}" + ". " + "#{people}"
-     current_line << customers
+  else
+    other_deli = []
+    other_deli.each_with_index do |people, index| 
+      customers= "The line is currently: #{index+1}" + ". " + "#{people}"
+      current_line << customers
   end
 end
 end
@@ -15,11 +15,13 @@ end
 def take_a_number
   
 end
+
 if line.length = 0) {     
   return `The line is currently empty.`;   
 } 
   else {      var array = [];     
-    for (var i = 0; i < line.length; i++) 
+    for (var i = 0; 
+    i < line.length; i++) 
     {       array.push(` ${i+1}. ${line[i]}`);     }     
     return `The line is currently:` + array;   } }
 
