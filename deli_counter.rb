8c4c435929f4db6cd3e93524d3ab98fef78b936b
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 12:33:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   puts "The line is currently empty."
   else
     current_line = []
-    other_deli.each_with_index do |people, index|
+    katz_deli.each_with_index do |people, index|
       current_line.push("#{index+1}" + ". " + "#{people}")
   end
   puts "The line is currently: " + current_line.join(" ")
